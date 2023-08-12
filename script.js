423712dc6a3aaab2695843720a108d7d8b294c64
mkorrea <mathscorrea@gmail.com>
2023-08-12 14:42:00 -0300

    ajuste de tamanho e rolagem

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".image-scroll-container");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+container.addEventListener("mousedown", (e) => {
+  isDown = true;
+  container.classList.add("active");
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener("mouseleave", () => {
+  isDown = false;
+  container.classList.remove("active");
+});
+
+container.addEventListener("mouseup", () => {
+  isDown = false;
+  container.classList.remove("active");
+});
+
+container.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * 1; // Ajuste a sensibilidade do arraste
+  container.scrollLeft = scrollLeft - walk;
+});
