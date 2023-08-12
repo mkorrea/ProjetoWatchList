b3411c8069daa5b69f20b9ad085b84b48d797ab1
mkorrea <mathscorrea@gmail.com>
2023-08-12 15:59:11 -0300

    ajuste de js na rolagem lateral

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-const container = document.querySelector(".image-scroll-container");
-let isDown = false;
-let startX;
-let scrollLeft;
+const containers = document.querySelectorAll(".image-scroll-container");
 
-container.addEventListener("mousedown", (e) => {
-  isDown = true;
-  container.classList.add("active");
-  startX = e.pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
-});
+containers.forEach(container => {
+  let isDown = false;
+  let startX;
+  let scrollLeft;
 
-container.addEventListener("mouseleave", () => {
-  isDown = false;
-  container.classList.remove("active");
-});
+  container.addEventListener("mousedown", (e) => {
+    isDown = true;
+    container.classList.add("active");
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
 
-container.addEventListener("mouseup", () => {
-  isDown = false;
-  container.classList.remove("active");
-});
+  container.addEventListener("mouseleave", () => {
+    isDown = false;
+    container.classList.remove("active");
+  });
 
-container.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - container.offsetLeft;
-  const walk = (x - startX) * 1; // Ajuste a sensibilidade do arraste
-  container.scrollLeft = scrollLeft - walk;
+  container.addEventListener("mouseup", () => {
+    isDown = false;
+    container.classList.remove("active");
+  });
+
+  container.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 1; // Ajuste a sensibilidade do arraste
+    container.scrollLeft = scrollLeft - walk;
+  });
 });
