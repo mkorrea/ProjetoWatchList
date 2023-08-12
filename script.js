91296795b98c602252a884ceee0fa85f68f1e7d6
mkorrea <mathscorrea@gmail.com>
2023-08-12 17:55:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 const containers = document.querySelectorAll(".image-scroll-container");
 
 containers.forEach(container => {
-  let isDown = false;
-  let startX;
-  let scrollLeft;
+let isDown = false;
+let startX;
+let scrollLeft;
 
-  container.addEventListener("mousedown", (e) => {
-    isDown = true;
-    container.classList.add("active");
-    startX = e.pageX - container.offsetLeft;
-    scrollLeft = container.scrollLeft;
-  });
+container.addEventListener("mousedown", (e) => {
+  isDown = true;
+  container.classList.add("active");
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
 
-  container.addEventListener("mouseleave", () => {
-    isDown = false;
-    container.classList.remove("active");
-  });
+container.addEventListener("mouseleave", () => {
+  isDown = false;
+  container.classList.remove("active");
+});
 
-  container.addEventListener("mouseup", () => {
-    isDown = false;
-    container.classList.remove("active");
-  });
+container.addEventListener("mouseup", () => {
+  isDown = false;
+  container.classList.remove("active");
+});
 
-  container.addEventListener("mousemove", (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - container.offsetLeft;
-    const walk = (x - startX) * 1; // Ajuste a sensibilidade do arraste
-    container.scrollLeft = scrollLeft - walk;
-  });
+container.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * 1; // Ajuste a sensibilidade do arraste
+  container.scrollLeft = scrollLeft - walk;
+});
 });
