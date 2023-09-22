28013165535d38cf4416675f8c710dbf1a05978c
mkorrea <mathscorrea@gmail.com>
2023-09-22 18:16:50 -0300

    rolagem lateral para celulares

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
 const containers = document.querySelectorAll(".image-scroll-container");
 
 containers.forEach(container => {
-let isDown = false;
-let startX;
-let scrollLeft;
+  let isDown = false;
+  let startX;
+  let scrollLeft;
 
-container.addEventListener("mousedown", (e) => {
-  isDown = true;
-  container.classList.add("active");
-  startX = e.pageX - container.offsetLeft;
-  scrollLeft = container.scrollLeft;
-});
+  container.addEventListener("mousedown", (e) => {
+    isDown = true;
+    container.classList.add("active");
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
 
-container.addEventListener("mouseleave", () => {
-  isDown = false;
-  container.classList.remove("active");
-});
+  container.addEventListener("touchstart", (e) => {
+    isDown = true;
+    container.classList.add("active");
+    startX = e.touches[0].pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
 
-container.addEventListener("mouseup", () => {
-  isDown = false;
-  container.classList.remove("active");
-});
+  container.addEventListener("mouseleave", () => {
+    isDown = false;
+    container.classList.remove("active");
+  });
 
-container.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - container.offsetLeft;
-  const walk = (x - startX) * 1; // Ajuste a sensibilidade do arraste
-  container.scrollLeft = scrollLeft - walk;
-});
+  container.addEventListener("mouseup", () => {
+    isDown = false;
+    container.classList.remove("active");
+  });
+
+  container.addEventListener("touchend", () => {
+    isDown = false;
+    container.classList.remove("active");
+  });
+
+  container.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 1; // Ajuste a sensibilidade do arraste
+    container.scrollLeft = scrollLeft - walk;
+  });
+
+  container.addEventListener("touchmove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.touches[0].pageX - container.offsetLeft;
+    const walk = (x - startX) * 1; // Ajuste a sensibilidade do arraste
+    container.scrollLeft = scrollLeft - walk;
+  });
 });
